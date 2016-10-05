f2ef2d560151522d3b7c14a8451bd148d7b1b3f0
Ben Murphy <ben@constellation.software>
2016-10-05 17:09:58 -0400

    Basic RAC implementation of search by ID
    Adding ViewModel

---
+++ DailyCombinator/HNViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  HNViewModel.swift
+//  DailyCombinator
+//
+//  Created by Ben Murphy on 10/5/16.
+//  Copyright © 2016 Constellation Software. All rights reserved.
+//
+
+import Foundation
+import ReactiveSwift
+import Firebase
